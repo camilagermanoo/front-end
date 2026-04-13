7fab0fa2e6ed98b18b5793a2a4b7f1c965adbb98
unknown <2414290090@academico.iesb.br>
2026-04-13 08:59:42 -0300

    adicionando codigos da pratica 3

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Dashboard from './pages/Dashboard';
+import Boletos from './pages/Boletos';
+import Faltas from './pages/Faltas';
+import Notas from './pages/Notas';
+import Requerimentos from './pages/Requerimentos';
 
 function App() {
   return <></>;
+  return <Dashboard />
 }
 
 export default App;
---
+++ praticas/aluno-online-react/src/components/Topbar.jsx
@@ -0,0 +1,12 @@
+import user from '../assets/avatar.svg';
+
+function Topbar() {
+    return (
+        <header>
+        <h1 >Olá, Aluno!</h1>
+        <img src={user} alt="imagem avatar" />
+      </header>
+    );
+}
+
+export default Topbar;
