bc8d851894968c9652964bcf041099bfecc982a1
camila germano rodrigues <camyrodrigues23@gmail.com>
2026-06-08 10:35:30 -0300

    arrumando codigos da pratica8

--- praticas/aluno-online-react/src/components/Topbar.jsx
+++ praticas/aluno-online-react/src/components/Topbar.jsx
@@ -1,12 +1,19 @@
-import user from '../assets/avatar.svg';
+import user from "../assets/avatar.svg";
 
-function Topbar(props) {
-    return (
-        <header className='flex justify-between items-center py-[26px]'>
-        <h1 className='text-[2rem] font-bold m-4 mb-0'>{props.titulo}</h1>
-        <img className="h-[70px] w-[70px] mr-4" src={user} alt="imagem avatar" />
-      </header>
-    );
+function Topbar({ titulo }) {
+  return (
+    <header className="flex justify-between items-center py-[26px]">
+      <h1 className="text-[2rem] font-bold m-4 mb-0">
+        {titulo}
+      </h1>
+
+      <img
+        className="h-[70px] w-[70px] mr-4"
+        src={user}
+        alt="imagem avatar"
+      />
+    </header>
+  );
 }
 
 export default Topbar;
